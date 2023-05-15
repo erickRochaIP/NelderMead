d5bd67a7f90ce912aca8868fc0b0c8436b058d5d
Erick Rocha <erickfr1011@gmail.com>
2023-05-15 16:01:46 -0300

    Parametrizar coeficientes

--- nelder_mead.py
+++ nelder_mead.py
@@ -57,6 +57,10 @@ def contrair_simplex(S, n, coef, f):
 def nelder_mead(f, x0, avals, params = None):
 	# TODO: Receber ponto inicial e params
     n = len(x0)
+    coef_reflexao = 2
+    coef_exp = 4
+    coef_contracao = 1/2
+    coef_encolhimento = 1/2
     Simplex = criar_simplex(f, x0)
     Simplex = ordenar_simplex(Simplex)
     pior, segundo_pior, melhor = extrair_dados(Simplex)
@@ -64,13 +68,13 @@ def nelder_mead(f, x0, avals, params = None):
     while avals > 0:
         avals -= 1
         centroide = calcular_centroide(Simplex, n)
-        refletido = calcular_ponto_deslocado(pior[0], centroide, 2, f)
+        refletido = calcular_ponto_deslocado(pior[0], centroide, coef_reflexao, f)
         if segundo_pior[1] > refletido[1] >= melhor[1]:
             Simplex = substituir_pior_ponto(Simplex, refletido[0], refletido[1])
             pior, segundo_pior, melhor = extrair_dados(Simplex)
             continue
         if melhor[1] > refletido[1]:
-            expandido = calcular_ponto_deslocado(pior[0], centroide, 4, f)
+            expandido = calcular_ponto_deslocado(pior[0], centroide, coef_exp, f)
             if refletido[1] > expandido[1]:
                 Simplex = substituir_pior_ponto(Simplex, expandido[0], expandido[1])
             else:
@@ -78,19 +82,19 @@ def nelder_mead(f, x0, avals, params = None):
             pior, segundo_pior, melhor = extrair_dados(Simplex)
             continue
         if pior[1] >= refletido[1]:
-            contraido_externo = calcular_ponto_deslocado(pior[0], centroide, 3/2, f)
+            contraido_externo = calcular_ponto_deslocado(pior[0], centroide, coef_reflexao - coef_contracao, f)
             if refletido[1] > contraido_externo[1]:
                 Simplex = substituir_pior_ponto(Simplex, contraido_externo[0], contraido_externo[1])
                 pior, segundo_pior, melhor = extrair_dados(Simplex)
                 continue
         else:
-            contraido_interno = calcular_ponto_deslocado(pior[0], centroide, 1/2, f)
+            contraido_interno = calcular_ponto_deslocado(pior[0], centroide, coef_contracao, f)
             if pior[1] > contraido_interno[1]:
                 Simplex = substituir_pior_ponto(Simplex, contraido_interno[0], contraido_interno[1])
                 pior, segundo_pior, melhor = extrair_dados(Simplex)
                 continue
 
-        Simplex = contrair_simplex(Simplex, n, 1/2, f)
+        Simplex = contrair_simplex(Simplex, n, coef_encolhimento, f)
         pior, segundo_pior, melhor = extrair_dados(Simplex)
 
     return melhor
